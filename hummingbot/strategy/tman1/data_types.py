8117d147da8adfc8d3e27e171da15549b68e51e6
dfr <deflexor@yandex.ru>
2021-08-09 19:52:57 +0300

    ...

---
+++ hummingbot/strategy/tman1/data_types.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+Liquidity mining data types...
+"""
+
+from decimal import Decimal
+
+
+# class PriceSize:
+#     """
+#     Order price and order size.
+#     """
+#     def __init__(self, price: Decimal, size: Decimal):
+#         self.price: Decimal = price
+#         self.size: Decimal = size
+
+#     def __repr__(self):
+#         return f"[ p: {self.price} s: {self.size} ]"
+
+
+class Proposal:
+    def __init__(self, market: str, dir: PositionSide, price: Decimal, size: Decimal):
+        self.market: str = market
+        self.dir: PositionSide = dir
+        self.size: Decimal = size
+        self.price: Decimal = price
+        self.tp: Decimal = price + price * 0.02
+        self.sl: Decimal = price - price * 0.01
+        if dir == PositionSide.SHORT:
+            tp = self.tp
+            self.tp = self.sl
+            self.sl = tp
+
+    def __repr__(self):
+        return f"{self.market} sir: {self.dir} price: {self.price} tp: {self.tp} sl: {self.sl}"
+
+    def base(self):
+        return self.market.split("-")[0]
+
+    def quote(self):
+        return self.market.split("-")[1]
